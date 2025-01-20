35fa2c296f2ba3c2deb1ffa80af2688bc05c53d6
sanjidulshahin <114828129+sanjidulshahin@users.noreply.github.com>
2025-01-20 23:07:32 +0600

    Added my portfolio

---
+++ Scripts/scripts.js
@@ -0,0 +1,34 @@
+const form = document.querySelector("form");
+const fullname = document.getElementById("name");
+const phone = document.getElementById("phone");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+
+function sendEmail() {
+  const bodyMessage = `Full Name: ${fullname.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value} <br> Message: ${message.value}`;
+
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "sanjidulshahin@gmail.com",
+    Password: "5D96A839270996FFA1BFFB4C757D42DD4F17",
+    To: "sanjidulshahin@gmail.com",
+    From: email.value,
+    Subject: subject.value,
+    Body: bodyMessage,
+  }).then((message) => {
+    if (message == "OK") {
+      Swal.fire({
+        title: "Good job!",
+        text: "Your Email is sent!",
+        icon: "success",
+      });
+    }
+  });
+  form.reset();
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendEmail();
+});
